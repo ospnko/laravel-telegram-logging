03589b8634c3c7f5d9d909891e0109a15af5c878
artur.osipenko <artur.osipenko@hotmail.com>
2020-03-24 16:28:33 +0200

    feat: formatter

--- src/Handler.php
+++ src/Handler.php
@@ -9,7 +9,7 @@ use Monolog\Handler\AbstractProcessingHandler;
 
 class Handler extends AbstractProcessingHandler
 {
-    const API_DOMAIN = 'https://api.telegram.org';
+    const API_DOMAIN = 'https://api.telegram.org/bot';
 
     protected $token;
     protected $chatId;
@@ -22,20 +22,37 @@ class Handler extends AbstractProcessingHandler
             throw new Exception('Telegram logging configuration not found');
         }
 
-        $this->token = config('logging.channels.telegram.token');
+        $this->token  = config('logging.channels.telegram.token');
         $this->chatId = config('logging.channels.telegram.chat_id');
     }
 
     public function write($record): void
     {
-        $url = self::API_DOMAIN
-            . '/bot' . $this->token
-            . '/sendMessage';
+        $this->send($this->format($record));
+    }
 
-        Http::get($url, [
-            'text' => $record['formatted'],
+    public function send(string $message)
+    {
+        $url = self::API_DOMAIN . $this->token . '/sendMessage';
+
+        return Http::get($url, [
+            'text' => $message,
             'chat_id' => $this->chatId,
             'parse_mode' => 'html'
         ]);
+    }
+
+    public function format($record)
+    {
+        return "[{$record['level_name']}] <b>" . config('app.name') . "</b>\n"
+            . "———————————\n"
+            . "TIME: " . now()->toDateTimeString() . "\n"
+            . "ENV: {$record['channel']} \n"
+            . "URL: " . request()->url() . "\n"
+            . "IP: " . request()->ip() . "\n"
+            . "\n"
+            . "<b>{$record['message']}</b>\n"
+            . $record['context']['exception']->getFile()
+            . " [{$record['context']['exception']->getLine()}]";
     }
 }
